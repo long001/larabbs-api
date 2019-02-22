86ef5f4cf66bfa4d1fca08b9ae6dbd7e8a7f44d1
long <396667187@qq.com>
2019-02-22 02:09:00 +0000

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,19 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ], function($api) {
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
+    $api->group([
+        'middleware' => 'api.throttle',
+//        'limit' => 1,
+//        'expires' => 1,
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function($api) {
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
 
-    // 用户注册
-    $api->post('users', 'UsersController@store')
-        ->name('api.users.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+    });
 });
