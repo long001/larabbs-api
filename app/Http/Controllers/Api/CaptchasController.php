aad6d78b6731346cb67ef6adc19ce3cace48cb9a
long <396667187@qq.com>
2019-02-22 02:51:31 +0000

    add captcha

---
+++ app/Http/Controllers/Api/CaptchasController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Gregwar\Captcha\CaptchaBuilder;
+use App\Http\Requests\Api\CaptchaRequest;
+
+class CaptchasController extends Controller
+{
+    public function store(CaptchaRequest $request, CaptchaBuilder $captchaBuilder)
+    {
+        $key = 'captcha-'.str_random(15);
+        $phone = $request->phone;
+
+        $captcha = $captchaBuilder->build();
+        $expiredAt = now()->addMinutes(2);
+        \Cache::put($key, ['phone' => $phone, 'code' => $captcha->getPhrase()], $expiredAt);
+
+        $result = [
+            'captcha_key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+            'captcha_image_content' => $captcha->inline()
+            // 在 chrome 浏览器中 查看 base64 图片的方法：F12，在 console 中输入 base64 内容，点击生成的链接
+        ];
+
+        return $this->response->array($result)->setStatusCode(201);
+    }
+}
--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -11,6 +11,18 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
+        $captchaData = \Cache::get($request->captcha_key);
+
+        if (!$captchaData) {
+            return $this->response->error('图片验证码已失效', 422);
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
         $phone = $request->phone;
 
         if (!app()->environment('production')) {
@@ -34,6 +46,9 @@ class VerificationCodesController extends Controller
         $expiredAt = now()->addMinutes(10);
         // 缓存验证码 10分钟过期。
         \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        // 清除图片验证码缓存
+        \Cache::forget($request->captcha_key);
 
         return $this->response->array([
             'key' => $key,
